4c804aa93ba21807530aa69569403b6809f4a77e
Kyle Fang <fang.kyle@gmail.com>
2024-09-03 12:09:33 -0600

    fail test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+  "reflect"
+  "testing"
+  "net/http"
+)
+
+func TestGetAPIKey(t *testing.T) {
+  header := http.Header{}
+  header.Add("Authorization", "ApiKey hello-world")
+  got, err := GetAPIKey(header)
+  if err != nil {
+    t.Fatalf("unexpected error %v", err)
+  }
+  want := "hello-world-2"
+  if !reflect.DeepEqual(want, got) {
+    t.Fatalf("expected: %v, got: %v", want, got)
+  }
+}
